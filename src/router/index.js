847737f4e5741603abee05d24196ae15bec72619
youssef <youssefatef@gmail.com>
2024-06-03 12:27:58 +0300

    lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import MovieDetail from '../views/MovieDetail.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+// component: () => import("../views/Home.vue"),
+// This type of import for lazy loading
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: () => import("../views/Home.vue"),
   },
   {
-    path: '/movie/:id',
-    name: 'Movie Detail',
-    component: MovieDetail
-  }
-]
+    path: "/movie/:id",
+    name: "Movie Detail",
+    component: () => import("../views/MovieDetail.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
-export default router
+export default router;
+
